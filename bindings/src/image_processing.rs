151f48b8552de88c301f60a495ac1bbd3f046d7b
Andrzej Pisarek <andrzej.r.pisarek@gmail.com>
2019-07-25 16:46:19 +0200

    Add add_watermark function

--- bindings/src/image_processing.rs
+++ bindings/src/image_processing.rs
@@ -1,4 +1,4 @@
-use image::DynamicImage;
+use image::{GenericImageView, DynamicImage, ImageBuffer, Rgba, ImageRgba8};
 
 pub fn flip_horizontally(img: DynamicImage) -> DynamicImage {
     let filtered = img.fliph();
@@ -25,4 +25,41 @@ pub fn pixellate(img: DynamicImage) -> DynamicImage {
     let rgbImage = img.to_rgb();
 
     img
+}
+
+
+pub fn add_watermark(
+    original_img: DynamicImage,
+    watermark_img: DynamicImage,
+    transparency: f32
+) -> DynamicImage {
+    // adds watermark in top left corner
+    // original source image should be bigger than logo, otherwise it will crash
+    // with transparency 0.0 the logo will be fully opaque
+    // with transparency 1.0 the logo will be invisible
+    let (width, height) = watermark_img.dimensions();
+    let mut img_buffer = original_img.to_rgba();
+    let watermark_buffer = watermark_img.to_rgba();
+
+    for x in 0..width {
+        for y in 0..height {
+            let yelp_pixel = watermark_buffer.get_pixel(x, y);
+
+            let yelp_alpha = yelp_pixel[3];
+            let is_opaque = yelp_alpha != 0;
+
+            if is_opaque {
+                let mut new_pixel = *img_buffer.get_pixel(x, y);
+
+                for channel in 0..3 {
+                    new_pixel[channel] = (
+                        (new_pixel[channel] as f32) * transparency + (yelp_pixel[channel] as f32) * (1.0 - transparency)
+                    ) as u8;
+                }
+
+                img_buffer.put_pixel(x, y, new_pixel);
+            }
+        }
+    }
+    ImageRgba8(img_buffer)
 }
