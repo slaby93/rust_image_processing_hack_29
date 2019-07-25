42032baec797ced4d5d6bd32d659bf13ca86b6c0
Daniel Slaby <pufek93@gmail.com>
2019-07-25 14:07:24 +0200

    Decode and encode base64 text

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
-
+extern crate base64;
+extern crate image;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -17,4 +18,13 @@ extern {
 pub fn greet(text: &str) {
     let text_to_display = format!("Your input: {}", text);
     alert(&text_to_display);
+}
+
+const IMAGE_PREFIX: &str = "data:image/png;base64,";
+
+#[wasm_bindgen]
+pub fn load_image(image_base_64: &str) {
+    utils::set_panic_hook();
+    let decoded_image = base64::decode("dGVzdA==").unwrap();
+    alert(&format!("X: {:?}", String::from_utf8(decoded_image).unwrap()));
 }
