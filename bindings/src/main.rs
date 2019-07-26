d2a4bf437fe87ef086af29e2bb1a9839a0691041
Benoit Vermont <bvermont@yelp.com>
2019-07-26 10:56:49 +0200

    Use jpg instead of pngs for test outputs

--- bindings/src/main.rs
+++ bindings/src/main.rs
@@ -2,13 +2,24 @@ use std::fs;
 
 mod image_processing;
 
-fn main(){
+fn main() {
     let img = image::open("sample/fuji.jpg").unwrap();
     let watermark = image::open("sample/watermark.png").unwrap();
 
-    fs::create_dir_all("outputs");
-    image_processing::grayscale(&img).save("outputs/grayscale.png");
-    image_processing::add_watermark(&img, &watermark, 0.5).save("outputs/watermark.png");
-    image_processing::detect_edges(&img).save("outputs/detect_edges.png");
-    image_processing::add_noise(&img).save("outputs/add_noise.png");
+    fs::create_dir_all("outputs").unwrap();
+    image_processing::grayscale(&img)
+        .save("outputs/grayscale.jpg")
+        .unwrap();
+    image_processing::add_watermark(&img, &watermark, 0.5)
+        .save("outputs/watermark.jpg")
+        .unwrap();
+    image_processing::detect_edges(&img)
+        .save("outputs/detect_edges.jpg")
+        .unwrap();
+    image_processing::add_noise(&img)
+        .save("outputs/add_noise.jpg")
+        .unwrap();
+    image_processing::invert(&img)
+        .save("outputs/invert.jpg")
+        .unwrap();
 }
