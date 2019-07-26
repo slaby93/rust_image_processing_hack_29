f78a4cc626be8953d6b58c22ead3cd41f7bcfca3
Ivan Tkachenko <itkachen@yelp.com>
2019-07-26 10:17:07 +0200

    Added edges filter

--- bindings/src/main.rs
+++ bindings/src/main.rs
@@ -9,4 +9,5 @@ fn main(){
     fs::create_dir_all("outputs");
     image_processing::grayscale(&img).save("outputs/grayscale.png");
     image_processing::add_watermark(&img, &watermark, 0.5).save("outputs/fuji_and_watermark.png");
+    image_processing::detect_edges(&img).save("outputs/detect_edges.png");
 }
