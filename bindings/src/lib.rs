e4098cfcf1a7293409db76964dc7d7786951a5eb
Daniel Slaby <pufek93@gmail.com>
2019-07-26 10:06:19 +0200

    Fix lib.rs

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 extern crate base64;
 extern crate image;
+mod image_processing;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
